d13b8781770698261bee156990dbfe18f119c52c
Joel Red <joshua-lunag@hotmail.com>
2025-07-02 22:46:37 -0500

    Taller 33

--- src/main/java/uce/edu/web/api/controller/Estudiantecontroller.java
+++ src/main/java/uce/edu/web/api/controller/Estudiantecontroller.java
@@ -1,11 +1,10 @@
 package uce.edu.web.api.controller;
 
-import java.util.List;
-
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PATCH;
@@ -13,7 +12,10 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import uce.edu.web.api.repository.modelo.Estudiante;
 import uce.edu.web.api.service.IEstudianteService;
 
@@ -25,39 +27,52 @@ public class Estudiantecontroller {// se lo suele llamar el servicion - web serv
 
     @GET
     @Path("/{id}") // path param se usa para consultar un recurso mediante su id
-    public Estudiante consultarPorId(@PathParam("id") Integer id) { // se lo suele llamar capacidades
-        return this.estudianteService.buscarPorId(id);
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Consultar por ID", description = "Nos permiete permite conultar un elemnto de la tabla estudiante mediatne un id")
+    public Response consultarPorId(@PathParam("id") Integer id) { // se lo suele llamar capacidades
+
+        return Response.status(Response.Status.OK).entity(this.estudianteService.buscarPorId(id)).build();
 
     }
 
     // ?genero=F&provincia=pichincha
     @GET
     @Path("")
-    @Operation(summary = "conusltar estudiantes")
-    public List<Estudiante> consultarTodos(@QueryParam("genero") String genero,
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "conusltar estudiantes", description = "Nos permite ver todos los elementos de la tabla estudiante")
+    public Response consultarTodos(@QueryParam("genero") String genero,
             @QueryParam("provincia") String provincia) {
         System.out.println(provincia);
-        return this.estudianteService.buscarTodos(genero);
+
+        return Response.status(Response.Status.OK).entity(this.estudianteService.buscarTodos(genero)).build();
     }
 
     @POST // Indica que este método maneja solicitudes POST
     @Path("")
+    @Consumes(MediaType.APPLICATION_JSON)
     @Operation(summary = "Guardar Estudiante", description = "Nos permiete guardar en la .....")
-    public void guardar(Estudiante estudiante) {
+    public Response guardar(Estudiante estudiante) {
         this.estudianteService.guardar(estudiante);
+        return Response.status(Response.Status.OK).build();
 
     }
 
     @PUT
     @Path("/{id}")
-    public void actualizar(@RequestBody Estudiante estudiante, @PathParam("id") Integer id) {
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Actualizar estudiante", description = "Nos permiete actualizar un estuidante especifico  mediante su id")
+    public Response actualizar(@RequestBody Estudiante estudiante, @PathParam("id") Integer id) {
         estudiante.setId(id);
         this.estudianteService.actualizarPorId(estudiante);
+        return Response.status(Response.Status.OK).build();
     }
 
     @PATCH
     @Path("/{id}")
-    public void actualizarParcial(@RequestBody Estudiante estudiante, @PathParam("id") Integer id) {
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Actualizar parcial de un estudiante", description = "Nos actualizar un estudiante especifico mediante su id")
+
+    public Response actualizarParcial(@RequestBody Estudiante estudiante, @PathParam("id") Integer id) {
         estudiante.setId(id);
         Estudiante e = this.estudianteService.buscarPorId(id);
         if (estudiante.getApellido() != null) {
@@ -70,13 +85,18 @@ public class Estudiantecontroller {// se lo suele llamar el servicion - web serv
         if (estudiante.getNombre() != null) {
             e.setNombre(estudiante.getNombre());
         }
-        this.estudianteService.actualizarParcialPorId(estudiante);
+        this.estudianteService.actualizarParcialPorId(e);
+        return Response.status(Response.Status.OK).build();
     }
 
     @DELETE
     @Path("/{id}")
-    public void borrarPorId(@PathParam("id") Integer id) {
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Borrar Estudiante", description = "Nos permiete eliminar un estudiante especifico mediante un id")
+
+    public Response borrarPorId(@PathParam("id") Integer id) {
         this.estudianteService.borrarPorId(id);
+        return Response.status(Response.Status.OK).build();
 
     }
 
